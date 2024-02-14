591a54e8bdb04541b1605a05c6e60a4490e5651d
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2024-02-13 20:40:13 -0500

    nav funcionando correctamente

--- code/js/scrool_nav.js
+++ code/js/scrool_nav.js
@@ -1,4 +1,14 @@
-window.addEventListener('scroll', ()=>{
-    let y = window.screenY
-    console.log(y);
+window.addEventListener('scroll', () => {
+    const nav = document.getElementById('nav');
+    const distancia = nav.getBoundingClientRect().top;
+
+    if (distancia <= 0 && window.scrollY > 100) {
+        nav.classList.add('fixed', 'top-0', 'items-center', 'bg-opacity-85');
+        nav.classList.remove('items-end');
+    } else {
+        nav.classList.remove('fixed', 'top-0', 'items-center', 'bg-opacity-85');
+        nav.classList.add('items-end');
+    }
+
+    console.log(window.scrollY);
 })
